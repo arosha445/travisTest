63586785918f6454b1259748c7b761c95a36391a
Arosha De Silva <arosha@appletechlabs.com>
2018-04-25 23:15:46 +0530

    Your Message

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -7,7 +7,7 @@ class BasicTest extends TestCase
     public function testMain()
 	{
         $this->assertEquals(getenv('testVariable'),101);
-        $this->assertEquals(getenv('testVariable'),102);
+        $this->assertNotEquals(getenv('testVariable'),102);
     }
 }
 
